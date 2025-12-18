a62a8f07aca0ccae6a2460f6bfb17ae4759a4a1b
bekaduelmila2008 <bekbolot.abdykadyrov.2008@gmail.com>
2025-12-18 10:43:21 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,18 +1,25 @@
 console.log ("Inizio esecuzione");
 let d = new Date();
 console.log(d);
-getElemenrìtById("saluto").innerHTML = d + "è un bel giorno";
+getElementById("saluto").innerHTML = d + "è un bel giorno";
 
 function elabora(){
     let numero = document.getElementById("inNumber").value;
     alert(numero);
 }
 if (numero >= 0 && numero <= 255) {
+    colori.push(numero);
     documentElementById("outText").innerHTML = "Hai inserito il" + "numero" + numero;
+    documentElementById("outText").innerHTML +=  "<br> I colori inseriti sono:" + colori;
 } else {
     document.getElementById("error").innerHTML = "Il numero non è valido";
+    document.getElementById("outText").innerHTML="";
+    console.log(colori);
 }
 
 function reset() {
     document.getElementById("InNumber").value = "";
+    document.getElementById("outTExt").innerHTML = "";
+    document.getElementById("error").innerText = "";
 }
+
